3245a70ce73f9e2b130ddad3185f23b226529d09
Aleksandra Adelinova Valchanova <alenceto0@gmail.com>
2023-09-15 17:30:41 +0300

    type string[] and its advantages

--- Getting Started/app.ts
+++ Getting Started/app.ts
@@ -4,7 +4,15 @@
 // } = {
 const person = {
     name: "Alex",
-    age: 24
+    age: 24,
+    hobbies: ['Tennis', 'Hiking']
 };
 
+let favouriteActivities: string[];
+favouriteActivities = ['Tennis'];
+
 console.log(person.name)
+
+for (const hobby of person.hobbies) {
+    console.log(hobby.toUpperCase())
+}
