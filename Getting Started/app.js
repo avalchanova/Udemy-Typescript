2cf3d9eceafc9ca9b0fadbb547f1cd0360a764de
Aleksandra Adelinova Valchanova <alenceto0@gmail.com>
2023-09-15 17:30:16 +0300

    compiled code

--- Getting Started/app.js
+++ Getting Started/app.js
@@ -1,5 +1,16 @@
+// const person: {
+//     name: string;
+//     age: number;
+// } = {
 var person = {
     name: "Alex",
-    age: 24
+    age: 24,
+    hobbies: ['Tennis', 'Hiking']
 };
+var favouriteActivities;
+favouriteActivities = ['Tennis'];
 console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase());
+}
