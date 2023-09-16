05ef580494b1de500c7136b63ae90f4a77a2d25a
Aleksandra Adelinova Valchanova <alenceto0@gmail.com>
2023-09-16 15:19:57 +0300

    enum type

--- Getting Started/app.ts
+++ Getting Started/app.ts
@@ -1,11 +1,22 @@
-const person: {
-    name: string;
-    age: number;
-    hobbies: string[];
-    role: [number, string] // a tuple: a fixed array
+// const person: {
+//     name: string;
+//     age: number;
+//     hobbies: string[];
+//     role: [number, string] // a tuple: a fixed array (not necessarily of two elements)
 
-} = {
-    // const person = {
+// } = {
+//     // const person = {
+//     name: "Alex",
+//     age: 24,
+//     hobbies: ['Tennis', 'Hiking'],
+//     role: [2, "Author"]
+// };
+
+enum Role { ADMIN, READ_ONLY, AUTHOR };
+// the enum type is a custom type therefore we write it with a capital letter
+// ADMIN is 0; READ_ONLY is 1 and so on like an index of an array
+
+const person = {
     name: "Alex",
     age: 24,
     hobbies: ['Tennis', 'Hiking'],
