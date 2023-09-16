e0acef2389241c151e4df8d8244d54342e584e82
Aleksandra Adelinova Valchanova <alenceto0@gmail.com>
2023-09-16 11:06:50 +0300

    tuple and push as na exception

--- Getting Started/app.ts
+++ Getting Started/app.ts
@@ -1,13 +1,22 @@
-// const person: {
-//     name: string;
-//     age: number;
-// } = {
-const person = {
+const person: {
+    name: string;
+    age: number;
+    hobbies: string[];
+    role: [number, string] // a tuple: a fixed array
+
+} = {
+    // const person = {
     name: "Alex",
     age: 24,
-    hobbies: ['Tennis', 'Hiking']
+    hobbies: ['Tennis', 'Hiking'],
+    role: [2, "Author"]
 };
 
+// person.role[1] = 10 // this blows an error because the second element is fixed as a string
+// person.role.push('admin') // this changes the tuple an is somehow acceptable even though it violates
+// the requirement two be an array of a number and a string: two els in total
+// push is a loophole
+// person.role = [2,"Author", "Admin"] // is not accepted and will also blow an error
 let favouriteActivities: string[];
 favouriteActivities = ['Tennis'];
 
