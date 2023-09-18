cb302f70dbd73cea8355fb3c17a474fcd73dac9f
Aleksandra Adelinova Valchanova <alenceto0@gmail.com>
2023-09-18 06:43:28 +0300

    aliases type as an object

--- Typescript Basics and Basic Types/app.js
+++ Typescript Basics and Basic Types/app.js
@@ -1,4 +1,8 @@
+// we usually use type Aliases in addition to type Union
+// 
 function combine(n1, n2, typeParams) {
+    // genius 
+    // we mix UNION AND LITERAL types
     var result;
     if (typeof n1 === "number" && n2 === "number" || typeParams === "as-number") {
         result = +n1 + +n2;
@@ -14,3 +18,11 @@ var combinedAgesAsStrings = combine("5", "7", "as-number");
 console.log(combinedAges);
 var combinedNames = combine('max', 'anne', "as-string");
 console.log(combinedNames);
+var u1 = { name: 'Max', age: 30 }; // this works!
+console.log("".concat(u1.name, " is ").concat(u1.age, " years old"));
+function greet(user) {
+    console.log('Hi, I am ' + user.name);
+}
+function isOlder(user, checkAge) {
+    return checkAge > user.age;
+}
--- Typescript Basics and Basic Types/app.ts
+++ Typescript Basics and Basic Types/app.ts
@@ -1,5 +1,6 @@
 type Combinable = number | string
-// we usually use type Alias in addition to type Union
+// we usually use type Aliases in addition to type Union
+// 
 
 function combine(
     n1: Combinable,
@@ -24,3 +25,34 @@ console.log(combinedAges)
 
 const combinedNames = combine('max', 'anne', "as-string");
 console.log(combinedNames);
+
+// Type aliases can be used to "create" your own types. 
+// You're not limited to storing union types though - you can also provide
+// an alias to a (possibly complex) object type.
+
+type User = { name: string; age: number };
+const u1: User = { name: 'Max', age: 30 }; // this works!
+console.log(`${u1.name} is ${u1.age} years old`)
+
+
+// THIS:
+
+// function greet(user: { name: string; age: number }) {
+//     console.log('Hi, I am ' + user.name);
+// }
+
+// function isOlder(user: { name: string; age: number }, checkAge: number) {
+//     return checkAge > user.age;
+// }
+
+// BECOMES THIS: 
+
+type UserObj = { name: string; age: number };
+
+function greet(user: UserObj) {
+    console.log('Hi, I am ' + user.name);
+}
+
+function isOlder(user: UserObj, checkAge: number) {
+    return checkAge > user.age;
+}
