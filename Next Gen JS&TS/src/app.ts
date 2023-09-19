92696fb76a3a66cc63fb192e16d1dbddfc264373
Aleksandra Adelinova Valchanova <alenceto0@gmail.com>
2023-09-19 13:29:50 +0300

    variations of arrow functions

--- Next Gen JS&TS/src/app.ts
+++ Next Gen JS&TS/src/app.ts
@@ -3,3 +3,16 @@ const add = (a:number, b:number) => {
 }
 
 console.log(add(2,5))
+
+const printOutput = (output: string | number)=> {
+    console.log(output)
+}
+
+const printOutputAgain: (a:number|string)=> void = output => console.log(output)
+
+const button = document.querySelector('button')
+if (button){
+    button.addEventListener('click', event => console.log(event))
+}
+
+printOutput(add(5,9))
