a68c0ff5d335d9e3f38c7dcdbefcbc7000ede314
Aleksandra Adelinova Valchanova <alenceto0@gmail.com>
2023-09-19 13:39:09 +0300

    spread operator (...)

--- Next Gen JS&TS/src/app.ts
+++ Next Gen JS&TS/src/app.ts
@@ -1,3 +1,4 @@
+// ARROW FUNCTIONS and default argument
 const add = (a:number, b:number=3) => {
     return a+b
 }
@@ -22,3 +23,26 @@ if (button){
 }
 
 printOutput(add(5,9))
+
+// THE SPREAD OPERATOR (...)
+
+const hobbies = ["Football", "Cooking"]
+const activeHobbies = ["Hiking"]
+
+// activeHobbies.push(hobbies) // this will push an array in the array which is not okay
+activeHobbies.push(hobbies[0], hobbies[1]) // that is okay but too long
+activeHobbies.push(...hobbies) 
+// pull out all the elements of the hobbies array 
+// and add them in the activeHobbies array
+
+const person = {
+    name: 'Max',
+    age:30
+};
+
+// const copiedPerson = person //we are coping the pointer at the Person object
+// which only points the copiedPerson obj to the memory where the Person obj is stored
+// not really creating a copy 
+
+const copiedPerson = {...person} // pulls out all the key-value pairs and they are 
+// added in the new copiedPerson object and it makes a perfect copy this way
