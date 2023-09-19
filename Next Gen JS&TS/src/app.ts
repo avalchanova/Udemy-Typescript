c6e7e0f9bde720cc8394ac615af0678d975586be
Aleksandra Adelinova Valchanova <alenceto0@gmail.com>
2023-09-19 13:32:16 +0300

    default arguments: always must be last

--- Next Gen JS&TS/src/app.ts
+++ Next Gen JS&TS/src/app.ts
@@ -1,11 +1,14 @@
-const add = (a:number, b:number) => {
+const add = (a:number, b:number=3) => {
     return a+b
 }
+// we can add default arguments like the b in that case but 
+// they must always be the last argument in the list, we cannot
+// put the b argument at the front
 
 const addAgain = (a:number, b:number) =>  a+b
 
 
-console.log(add(2,5))
+console.log(add(2))
 
 const printOutput = (output: string | number)=> {
     console.log(output)
