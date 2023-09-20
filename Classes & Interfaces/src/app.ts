961b0447644fa0a71f2de6e2a4a6502d4ded4f04
Aleksandra Adelinova Valchanova <alenceto0@gmail.com>
2023-09-20 11:27:47 +0300

    class syntax

--- Classes & Interfaces/src/app.ts
+++ Classes & Interfaces/src/app.ts
@@ -1,1 +1,11 @@
-// Code goes here!
+class Department {
+    name: string; // looks like an object but this is not a key-value, here we only we name the key we will use later and the type of that key 
+    // name is a property
+    constructor(n: string) { // this is a method --> any function in classes are called methods
+        this.name = n; 
+    }
+}
+
+const accounting = new Department('Accounting'); // this creates a new Department object
+
+console.log(accounting);
