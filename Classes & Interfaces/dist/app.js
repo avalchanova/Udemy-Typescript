073b9d9dc8011dcc35e9654ea151cb5593609f05
Aleksandra Adelinova Valchanova <alenceto0@gmail.com>
2023-09-20 12:27:22 +0300

    private properties and methods

--- Classes & Interfaces/dist/app.js
+++ Classes & Interfaces/dist/app.js
@@ -1,15 +1,25 @@
 "use strict";
 var Department = (function () {
     function Department(n) {
+        this.employees = [];
         this.name = n;
     }
     Department.prototype.describe = function () {
         console.log('Department: ' + this.name);
     };
+    Department.prototype.addEmployee = function (employee) {
+        this.employees.push(employee);
+    };
+    Department.prototype.printEmployeeInformation = function () {
+        console.log("Length: " + this.employees.length);
+        console.log(this.employees);
+    };
     return Department;
 }());
 var accounting = new Department('Accounting');
+accounting.addEmployee('Max');
+accounting.addEmployee('Manuel');
 accounting.describe();
+accounting.printEmployeeInformation();
 var accountingCopy = { name: 's', describe: accounting.describe };
-accountingCopy.describe();
 //# sourceMappingURL=app.js.map
--- Classes & Interfaces/src/app.ts
+++ Classes & Interfaces/src/app.ts
@@ -1,6 +1,10 @@
 class Department {
     name: string; // looks like an object but this is not a key-value, here we only we name the key we will use later and the type of that key 
     // name is a property
+    private employees: string[] = []
+
+    // keyword "private" can be used on properties and methods
+    // means that the prop/method is accessible only inside the created object
     constructor(n: string) { // this is a method --> any function in classes are called methods
         this.name = n; 
     }
@@ -11,20 +15,42 @@ class Department {
       console.log('Department: ' + this.name)  
     // always using the this keyword 
     }
+    
+    addEmployee(employee: string){
+        this.employees.push(employee)
+    }
+
+    printEmployeeInformation(){
+        console.log("Length: " + this.employees.length)
+        console.log(this.employees);
+    }
 }
 
 const accounting = new Department('Accounting'); // this creates a new Department object
 // this accounting is an instance of the Department class
 
+accounting.addEmployee('Max');
+accounting.addEmployee('Manuel');
+
+// accounting.employees[3] = "Anne"
+// another way we can access the employees without a method 
+// we can see that if we put it in a further index place it will leave
+// an "empty" space for the missed index positions
+// having two approaches is not preferred because when many people
+// are working on a project consistency is good
+// that is why we made this property private so it can be accessed only
+// inside the instance of the class and not outside
+
 // Calling the method of the class instance "Accounting"
-accounting.describe()
+accounting.describe();
+accounting.printEmployeeInformation();
 
 const accountingCopy = {name: 's', describe: accounting.describe}
 // when we add a name prop in here, because now TS sees that the object
 // on which we are calling the describe method now has a name prop just
 // like "this" expects it to have because it is based on the Department class
 
-accountingCopy.describe()
+// accountingCopy.describe()
 // this will console undefined because 
 // because it relies on the instance of the class (accounting)
 // and since the accountingCopy has no name, when this is called
