5ca355ec741d40d84604245e30ad107c9d8b607e
Aleksandra Adelinova Valchanova <alenceto0@gmail.com>
2023-09-25 13:41:11 +0300

    intersection types

---
+++ Advanced Types/app.ts
@@ -0,0 +1,21 @@
+type Admin = {
+  // difing a type
+  name: string;
+  privileges: string[];
+};
+
+type Employee = {
+  name: string;
+  startDate: Date;
+};
+
+type ElevatedEmployee = Admin & Employee; // the result is a new obj type that encapsulated Admin and Employee
+
+// we can achieve the same if instead of types the Admin and the Employee are interfaces
+// then we will create an interface ElevatedEmployee extends Admin, Employee
+// and the e1 will give the same result
+const e1: ElevatedEmployee = {
+  name: "Alex",
+  privileges: ["create-server"],
+  startDate: new Date(),
+};
