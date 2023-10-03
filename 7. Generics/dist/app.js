b9aa0b254e9ce5c475620a87074e13df454f05d9
Aleksandra Adelinova Valchanova <alenceto0@gmail.com>
2023-10-03 16:28:11 +0300

    keyof constraint

--- 7. Generics/dist/app.js
+++ 7. Generics/dist/app.js
@@ -6,4 +6,22 @@ function merge(objA, objB) {
 const mergedObj = merge({ name: 'Alex', hobbies: ["Horse riding", "Tennis"] }, { age: 24 });
 const mergedObj1 = merge({ name: 'Alex', hobbies: ["Horse riding", "Tennis"] }, { age: 24 });
 console.log(mergedObj1.hobbies[1]);
+console.log(mergedObj.age);
+function countAndDescribe(element) {
+    let descriptionText = "Got no value.";
+    if (element.length === 1) {
+        descriptionText = "Got 1 element.";
+    }
+    else if (element.length > 1) {
+        descriptionText = "Got " + element.length + " elements.";
+    }
+    return [element, descriptionText];
+}
+console.log(countAndDescribe("I have elements."));
+console.log(countAndDescribe(["Cooking", "Eating", "Working out"]));
+console.log(countAndDescribe([]));
+function extractAndConvert(obj, key) {
+    return "Value: " + obj[key];
+}
+console.log(extractAndConvert({ name: 'Alex' }, 'name'));
 //# sourceMappingURL=app.js.map
--- 7. Generics/src/app.ts
+++ 7. Generics/src/app.ts
@@ -19,16 +19,54 @@ const names: Array<string> = []; // completely the same as: string[]
 //     data.split(' ')
 // })
 
-// Generic function: 
+// Generic Function: 
     function merge<T extends Object, U extends Object>(objA: T, objB: U){
+        // in this case we write extends Object, but it can extend:
+        // - string, string || number, Animal (our own type), or we can skip extends and just write T
         // the convention is a single characater and usually the first one is T and then we go on with the alphabet letters
         return Object.assign(objA, objB);
     }
-    // here we say that generic type T can by anything
-    // and generic type U can also be anything but most likely it will be different from T
+    // here we say that generic type T can by any type of object 
+    // and generic type U can also be any type of object but most likely it will be different from T
 
 const mergedObj = merge<{name:string, hobbies: string[]}, {age:number}>({name:'Alex', hobbies:["Horse riding", "Tennis"]}, {age:24});
 // we can write it like that but it is completely unnecessary since TS knows what's up 
 const mergedObj1 = merge<{name:string, hobbies: string[]}, {age:number}>({name:'Alex', hobbies:["Horse riding", "Tennis"]}, {age:24});
 
-console.log(mergedObj1.hobbies[1]); 
+console.log(mergedObj1.hobbies[1]);
+console.log(mergedObj.age);
+// so basically with generic types we pass some additional info to the function
+// in order to work better with the result of the function
+
+interface Lengthy {
+    length: number;
+}
+
+function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
+    // we ensure that the function returns a tuple value -> an array with generic type T and a string
+    // but i do not understand how the Lengthy interface is extended?]
+    // maybe string, arrays, etc have property length out of the box (type number does not have!)
+    // and that is how we ensure the element will be of correct type 
+    let descriptionText = "Got no value."
+    if(element.length === 1){
+        descriptionText = "Got 1 element."
+    } else if (element.length > 1) {
+        descriptionText = "Got " + element.length + " elements.";
+    }
+    return [element, descriptionText]
+}
+
+console.log(countAndDescribe("I have elements.")); // counts the characters from the string
+console.log(countAndDescribe(["Cooking", "Eating", "Working out"])); // counts the elements in the array
+console.log(countAndDescribe([])); // empty array
+
+// the "keyof" Constraint
+
+function extractAndConvert<T extends object, U extends keyof T>(
+    obj: T, 
+    key: U
+) {
+    return "Value: " + obj[key];
+}
+
+console.log(extractAndConvert({name: 'Alex'}, 'name'));
