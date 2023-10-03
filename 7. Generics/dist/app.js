0e91a518894dd6c938bf0f2e218c5b644387329c
Aleksandra Adelinova Valchanova <alenceto0@gmail.com>
2023-10-03 15:28:01 +0300

    generic functions: adding <> after the function name and providing type

--- 7. Generics/dist/app.js
+++ 7. Generics/dist/app.js
@@ -1,3 +1,8 @@
 "use strict";
-const names = ["Alex", "Anna"];
+const names = [];
+function merge(objA, objB) {
+    return Object.assign(objA, objB);
+}
+const mergedObj = merge({ name: 'Alex', hobbies: ["Horse riding"] }, { age: 24 });
+console.log(mergedObj.hobbies);
 //# sourceMappingURL=app.js.map
--- 7. Generics/src/app.ts
+++ 7. Generics/src/app.ts
@@ -7,14 +7,24 @@ const names: Array<string> = []; // completely the same as: string[]
 // it is always good to tell TS what type will be in the array, so we can use methods for the given type
 // names[0].split(' ')
 
-const promise: Promise<string> = new Promise((resolve, reject)=> {
+//const promise: Promise<string> = new Promise((resolve, reject)=> {
     // we say to TS that this promise will resolve to string Promise<string>
     // we get better type safety
-    setTimeout(()=>{
-        resolve('This is done!')
-    },2000);
-});
+//     setTimeout(()=>{
+//         resolve('This is done!')
+//     },2000);
+// });
+/* The Promise object represents the eventual completion (or failure) of an asynchronous operation and its resulting value.*/
+// promise.then(data=> {
+//     data.split(' ')
+// })
 
-promise.then(data=> {
-    data.split(' ')
-})
+// Generic function: 
+    function merge<T extends Object, U extends Object>(objA: T, objB: U){
+        return Object.assign(objA, objB);
+    }
+    // here we say that generic type T can by anything
+    // and generic type U can also be anything but most likely it will be different from T
+
+const mergedObj = merge({name:'Alex', hobbies:["Horse riding"]}, {age:24});
+console.log(mergedObj.hobbies); 
