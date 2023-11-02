ab985a0e2352c62f9eced2965ec5513dcc55b823
Aleksandra Adelinova Valchanova <alenceto0@gmail.com>
2023-11-02 15:03:55 +0200

    theory on decorators

--- 8. Decorators/src/app.ts
+++ 8. Decorators/src/app.ts
@@ -1,1 +1,21 @@
-console.log('hiya');
+function Logger(constructor: Function){
+  // the only special thing in this case so far is the capital L in Logger, otherwise it is just a function
+  // but we can apply it as a decorator
+  // decorators receive arguments
+  console.log('Logging...');
+  console.log(constructor);
+}
+
+
+@Logger
+class Person {
+  name = "Alex";
+
+  constructor(){
+    console.log("Creating person object ...");
+  }
+}
+
+const pers = new Person();
+
+console.log(pers);
