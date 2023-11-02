b4d1930d0b25bee92b2d119e4a18ce6f8946fe17
Aleksandra Adelinova Valchanova <alenceto0@gmail.com>
2023-11-02 15:08:42 +0200

    theory on decorators

--- 8. Decorators/src/app.ts
+++ 8. Decorators/src/app.ts
@@ -2,6 +2,9 @@ function Logger(constructor: Function){
   // the only special thing in this case so far is the capital L in Logger, otherwise it is just a function
   // but we can apply it as a decorator
   // decorators receive arguments
+  // when a class is difined(like below) a decorator is executed
+  // not when it is instanciated
+  // so the decorator runs when JS finds the definition of a class that is using a decorator
   console.log('Logging...');
   console.log(constructor);
 }
