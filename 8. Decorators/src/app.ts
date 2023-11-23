3c1b17aa657bff85fdf23932f158117359d65038
Aleksandra Adelinova Valchanova <alenceto0@gmail.com>
2023-11-23 10:18:48 +0200

    log2

--- 8. Decorators/src/app.ts
+++ 8. Decorators/src/app.ts
@@ -65,6 +65,7 @@ function Log(target: any, propertyName: string | Symbol) {
 }
 
 function Log2(target: any, name: string, descriptor: PropertyDecorator) {
+console.log("Accessor decorator!");
 
 }
 
