aee9ef5b3d6417f5a31ded6f761e92a66439d685
Aleksandra Adelinova Valchanova <alenceto0@gmail.com>
2023-11-27 17:52:12 +0200

    successful course creation

--- 8. Decorators/dist/app.js
+++ 8. Decorators/dist/app.js
@@ -126,4 +126,14 @@ class Course {
         this.price = p;
     }
 }
+const courseForm = document.querySelector('form');
+courseForm.addEventListener('submit', event => {
+    event.preventDefault();
+    const titleEl = document.getElementById('title');
+    const priceEl = document.getElementById('price');
+    const title = titleEl.value;
+    const price = +priceEl.value;
+    const createdCourse = new Course(title, price);
+    console.log(createdCourse);
+});
 //# sourceMappingURL=app.js.map
