91c85a2587fce6648bce6c5d456378f3b862f71c
Aleksandra Adelinova Valchanova <alenceto0@gmail.com>
2023-11-28 15:22:57 +0200

    fixing the stored validator value in the array - in this case only required was stored, now is fixed

--- 8. Decorators/src/app.ts
+++ 8. Decorators/src/app.ts
@@ -180,8 +180,7 @@ const registeredValidators: ValidatorConfig = {};
 function Required(target: any, propName: string) {
     registeredValidators[target.constructor.name] = {
         ...registeredValidators[target.constructor.name], // this will add any existing key-value pairs
-        [propName]: ['required']
-    }
+        [propName]: [...(registeredValidators[target.constructor.name]?.[propName] ?? []), 'required']    }
     // this gives us the name of the class Course in this case
     // the protatype of the instance we are working with will have a constructor
     // key which points to the constructor function which was used to create our object
